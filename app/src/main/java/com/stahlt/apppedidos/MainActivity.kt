a5093346af5340c6295fc8b482b2a169fb35102f
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2024-05-31 15:00:10 -0300

    feat: add it as main screen

--- app/src/main/java/com/stahlt/apppedidos/MainActivity.kt
+++ app/src/main/java/com/stahlt/apppedidos/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.stahlt.apppedidos.ui.cliente.list.ClientesListScreen
 import com.stahlt.apppedidos.ui.theme.AppPedidosTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppPedidosTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ClientesListScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppPedidosTheme {
-        Greeting("Android")
     }
 }
